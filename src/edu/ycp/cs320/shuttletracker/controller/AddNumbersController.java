cfbb558d21859e3ce749ea4860b84666d4b02cb5
kmusco <kmusco@ycp.edu>
2016-04-28 20:47:22 -0400

    Clean Up
    Removed Guessing Game and Add Numbers demos

--- src/edu/ycp/cs320/shuttletracker/controller/AddNumbersController.java
+++
@@ -1,7 +0,0 @@
-package edu.ycp.cs320.shuttletracker.controller;
-
-public class AddNumbersController {
-	public Double add(Double first, Double second, Double third) {
-		return first + second + third;
-	}
-}
